24385c9e714b1e868cd3470173fbb87ff2201f8a
Manuel Klainer <manuelklainer@gmail.com>
2025-05-12 12:44:04 -0300

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,17 +1,11 @@
 def alguno_es_0(a : float, b : float) -> bool:
-	if (a==0 or b == 0):
-		return True
-	return False
+	return (a==0 or b == 0)
 
 def ambos_son_0(a : float, b : float) -> bool:
-	if (a==0 and b == 0):
-		return True
-	return False
+	return (a==0 and b == 0)
 
 def es_nombre_largo(nombre : str) -> bool:
 	return (3<=len(nombre) and 8>=len(nombre))
 
 def es_bisiesto (año : int) -> bool:
-	if (año % 400 == 0 or (año % 4 == 0 and año % 100 != 0)):
-		return True
-	return False
+	return (año % 400 == 0 or (año % 4 == 0 and año % 100 != 0))
