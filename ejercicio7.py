a7434e5c36e0f157be9e12ad553be796934dbecc
Manuel Klainer <manuelklainer@gmail.com>
2025-05-12 13:35:06 -0300

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,25 +1,25 @@
-def del_1_al_10():
+def del_1_al_10() -> None:
 	for i in range(1,11):
 		print(i)
 
-def pares_10_al_40():
+def pares_10_al_40() -> None:
 	for i in range(10,41,2):
 			print(i)
 
-def eco_10_veces():
+def eco_10_veces() -> None:
 	for i in range (10):
 		print ("eco")
 
-def cuenta_regresive(n: int):
+def cuenta_regresive(n: int) -> None:
 	for i in range (n, 0, -1):
 		print (i)
 	print("Despegue")
 
-def viaje_en_el_tiempo(partida: int, llegada: int):
+def viaje_en_el_tiempo(partida: int, llegada: int) -> None:
 	for i in range (partida -1 ,llegada -1, -1):
 		print(f"Viajó un año al pasado, estamos en el año: {i}")
 
-def viaje_al_pasado(partida: int):
+def viaje_al_pasado(partida: int) -> None:
 	for i in range (partida -20, -384 -1, -20):
 		print(f"Viajó 20 años al pasado, estamos en el año: {i}")
 
