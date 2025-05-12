ae6f6823d0d9cff72786f6361094a78fa8a591c4
Manuel Klainer <manuelklainer@gmail.com>
2025-05-12 12:33:15 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,12 +1,11 @@
-import math
-def peso_pino (altura : float):
+def peso_pino (altura : float) -> float:
 	altura_en_cm = altura * 100
 	base = min (altura_en_cm, 300) * 3
 	extra = max(altura_en_cm - 300, 0) * 2
 	return base + extra
 
-def es_peso_util (peso: float):
+def es_peso_util (peso: float) -> bool:
 	return min(peso,400) == 400 and max(peso,1000) == 1000
 
-def sirve_pino (altura: float):
+def sirve_pino (altura: float) -> bool:
 	return es_peso_util (peso_pino (altura))
