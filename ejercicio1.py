5c5fe0c3060feb9840b081a7806875c69c4a5134
Manuel Klainer <manuelklainer@gmail.com>
2025-05-12 12:32:12 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,14 +3,14 @@ def imprimir_hola_mundo():
 	print("¡Hola mundo!")
 
 def imprimir_un_verso():
-	print("No lo soñé-eheheh\nIbas corriendo a la deriba\nNo lo soñé-eheheh\nLos ojos ciegos bien abiertos\nNo mires por favor,\ny no prendas la luz\nLa imágen te disfiguró")
+	print("No lo soñé, yeh\nIbas corriendo a la deriva\nNo lo soñé, yeh\nLos ojos ciegos bien abiertos\nNo mires, por favor\nY no prendas la luz\nLa imagen te desfiguró")
 
-def raizDe2():
+def raizDe2() -> int:
 	return round(math.sqrt(2),4)
 
-def factorial_de_2():
+def factorial_de_2() -> int:
 	return 2
 
-def perimetro():
+def perimetro() -> float:
 	return 2 * math.pi
 
