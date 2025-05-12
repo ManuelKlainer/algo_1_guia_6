c252f4f44228b4892a7d60dd4fd44d15dc3bd5b8
Manuel Klainer <manuelklainer@gmail.com>
2025-05-12 12:32:30 -0300

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,21 +1,21 @@
 import math
-def imprimir_saludo (nombre: str):
+def imprimir_saludo (nombre: str)  -> str:
 	print ("Hola "+nombre)
 
-def raiz_cuadrada_de(n: float):
+def raiz_cuadrada_de(n: float) -> float:
 	return math.sqrt(n)
 
-def farenheit_a_celcius (t: float):
+def farenheit_a_celcius (t: float) -> float:
 	return ((t-32)*5)/9
 
-def imprimir_dos_veces(estribillo: str):
+def imprimir_dos_veces(estribillo: str) -> str:
 	return estribillo * 2
 
-def es_multiplo_de(n: int, m: int):
+def es_multiplo_de(n: int, m: int) -> bool:
 	return (n % m == 0)
 
-def es_par(n: int):
+def es_par(n: int) -> bool:
 	return es_multiplo_de(n,2)
 
-def cantidad_de_pizzas(comenzales: int, porciones: int):
+def cantidad_de_pizzas(comenzales: int, porciones: int) -> int:
 	return (comenzales*porciones + 8 - (comenzales * porciones) % 8) // 8
